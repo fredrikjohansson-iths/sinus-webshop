d0a5052db8a54e0bd6422db24d81a27ec55fd5fd
Stig Fredrik Johansson <fredrik.johansson@iths.se>
2021-03-01 11:27:27 +0100

    import files from bkup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,13 +8,17 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/about/',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    meta: {
+      title: 'Create your Sinus Webshop Account'
+    },
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignUp.vue')
   }
 ];
 
--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,13 +1,27 @@
 <template>
-  <div></div>
+  <div><SignUpForm @submitted="newUser" /></div>
 </template>
 
 <script>
+import axios from "axios";
+import SignUpForm from "/home/stig/git/sinus-webshop/frontend/src/components/SignUpForm.vue";
 export default {
-
-}
+  components: { SignUpForm },
+  methods: {
+    newUser(userData) {
+      axios
+        .post("http://localhost:5000/api/register/", userData)
+        .then(response => {
+          this.results = response.data;
+          this.$router.push("/");
+          console.log(response.status);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>

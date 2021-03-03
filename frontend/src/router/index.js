2c0bcbe22db4e2da27396bbc222d382716d8bb06
Stig Fredrik Johansson <fredrik.johansson@iths.se>
2021-03-03 19:59:44 +0100

    Cleaning lady

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,35 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Products from "../views/Products.vue";
-
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Home.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "../views/Home.vue")
   },
   {
     path: "/products",
     name: "Products",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Products.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Products.vue")
   },
-  
-   {
+
+  {
     path: "/customer-service",
     name: "Customer Service",
-  
-    component: () =>
-       import("../views/customer-service.vue")
-   },
-   {
+
+    component: () => import("../views/customer-service.vue")
+  },
+  {
     path: "/About-us",
     name: "About Us",
-   
-    component: () =>
-     import("../views/About-us.vue")
-   },
+
+    component: () => import("../views/About-us.vue")
+  },
 
   {
     path: "/checkout",
@@ -38,7 +33,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Checkout.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Checkout.vue")
   },
   {
     path: "/admin",
@@ -47,13 +42,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AdminCRUD.vue"),
-  },
+      import(/* webpackChunkName: "about" */ "../views/AdminCRUD.vue")
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;

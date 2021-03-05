2f5ea1171737ec87e22969fc3270bdfec41a3792
maddyhedman <71003013+maddyhedman@users.noreply.github.com>
2021-03-05 10:14:51 +0100

    | between the links in navbar

--- frontend/src/components/HeaderNavBar.vue
+++ frontend/src/components/HeaderNavBar.vue
@@ -6,9 +6,9 @@
       <!-- <router-link to="/MyAccount" v-if="userStatus" /> -->
 
       <router-link title="Admin configuration view" v-if="adminSession" to="/admin">Admin</router-link>
-
+|
       <a title="Sign in to Sinus" v-if="!userSession" @click="changeLoginModalStatus">Login</a>
-
+|
       <a class="shopping-cart-link" title="View your shopping cart" @click="changeCartStatus"
         >Shopping Cart ({{ shoppingCartLength }})</a
       >

8514ed655af4883dcb4fce9645b7b4b8952c0e81
maddyhedman <71003013+maddyhedman@users.noreply.github.com>
2021-03-09 16:39:35 +0100

    erades one components

--- frontend/src/views/Checkout.vue
+++ frontend/src/views/Checkout.vue
@@ -65,7 +65,7 @@ export default {
 <style lang="scss" scoped>
 #checkout {
   width: 80%;
-
+}
 
 .information {
   width: 60%;
@@ -74,6 +74,7 @@ export default {
   border-radius: 10px;
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
+
 .information {
   display: flex;
   justify-content: space-between;

b971b8f0ccd0ac30975cd957a66883ab3b0492e4
fredrikjohansson-iths <fredrik.johansson@iths.se>
2021-03-10 19:54:05 +0100

    added css to list

--- frontend/src/components/OrderList.vue
+++ frontend/src/components/OrderList.vue
@@ -2,10 +2,16 @@
   <div>
     <section class="single-order">
       <ul>
-        <li title="Click to expand" v-for="order in orders" :key="order._id" @click="showDetails">
-         <span class="point" :id="order._id"
-            >Order Id: {{ order._id }} Total: {{ order.orderValue }}SEK
+        <li
+          title="Click to expand"
+          v-for="order in orders"
+          :key="order._id"
+          @click="showDetails"
+        >
+          <span class="point" :id="order._id"
+            >Order Id: {{ order._id }} Total: {{ order.orderValue }} SEK
             <OrderListItem
+              class="item"
               v-show="show === order._id"
               v-for="(item, index) in order.items"
               :key="index"
@@ -54,12 +60,26 @@ export default {
   cursor: pointer;
 }
 
+.item {
+  background-color: black;
+  color: white;
+  font-weight: 600;
+  text-align: center;
+}
+
 .single-order {
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
 }
 li {
+  transition: scale 0.3s ease-in-out;
   list-style: none;
+  margin-top: 10px;
+  font-weight: 400;
+}
+
+li:hover {
+  scale: 1.1;
 }
 </style>
